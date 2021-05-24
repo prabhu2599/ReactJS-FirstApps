3f9a61012ce5bfa6ed9e689531becbfc23b583b7
prabhu2599 <js9884798839@gmai.com>
2021-05-24 14:51:38 +0530

    basic app first push

---
+++ src/Avatar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Avatar.css";
+
+const Avatar = () => {
+  return;
+  <div className="avatarstyle">
+    <img src="https://joeschmoe.io/api/v1/peachy" alt="AvatarPic" />
+    <h1>Peachy</h1>;<p>Sab kuch Karega</p>
+  </div>;
+};
+
+export default Avatar;
---
+++ src/demo.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import ReactDom from "react-dom";
+
+const Demo = (props) => {
+  return <h1>Heya &!!</h1>;
+};
+
+export default Demo;
+
+// agar class hai to {this.props} karo else {props.}
